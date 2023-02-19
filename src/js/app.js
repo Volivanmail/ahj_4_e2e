ab078574ebafe533c56abed939faf12586475a1e
Your Name <you@example.com>
2023-02-19 15:34:29 +0300

    fix in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,14 @@ import { Cards } from "./Cards";
 
 const visa = new Cards("visa", "../img/visa.png");
 const mastercard = new Cards("mastercard", "../img/mastercard.png");
-const americanExpress = new Cards(
-  "american_express",
-  "../img/american_express.png"
-);
 const discover = new Cards("discover", "../img/discover.png");
 const dinersClub = new Cards("diners_club", "../img/diners_club.png");
 const jcb = new Cards("JCB", "../img/jcb.png");
 const mir = new Cards("MIR", "../img/mir.png");
+const americanExpress = new Cards(
+  "american_express",
+  "../img/american_express.png"
+);
 
 visa.addCard();
 mastercard.addCard();
